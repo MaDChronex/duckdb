60634467241beba336b65ed7990625822bf25025
Robert Beierle <robertbeierle@MacBook-Pro-von-Robert-2.local>
2023-04-15 11:41:06 +0200

    test cases and made list_reverse(NULL) not nuke the whole system

--- src/function/scalar/list/list_reverse.cpp
+++ src/function/scalar/list/list_reverse.cpp
@@ -10,58 +10,56 @@ static void ListReverseFunction(DataChunk &args, ExpressionState &state, Vector 
 	D_ASSERT(args.ColumnCount() == 1);
 	auto count = args.size();
 
-	Vector &lhs = args.data[0];
+	Vector nul(0);
 
-	UnifiedVectorFormat lhs_data;
-	//UnifiedVectorFormat rhs_data;
-	lhs.ToUnifiedFormat(count, lhs_data);
-	// rhs.ToUnifiedFormat(count, rhs_data);
-	auto lhs_entries = (list_entry_t *)lhs_data.data;
-	// auto rhs_entries = (list_entry_t *)rhs_data.data;
+	Vector &arrlist = args.data[0];
+		if (arrlist.GetType().id() == LogicalTypeId::SQLNULL) {
+			result.SetVectorType(VectorType::CONSTANT_VECTOR);
+			ConstantVector::SetNull(result, true);
+			// result.Reference(nul); //should be NULL not 0
+		return;
+	}
 
-	auto lhs_list_size = ListVector::GetListSize(lhs);
-	// auto rhs_list_size = ListVector::GetListSize(rhs);
-	auto &lhs_child = ListVector::GetEntry(lhs);
-	// auto &rhs_child = ListVector::GetEntry(rhs);
-	UnifiedVectorFormat lhs_child_data;
-	// UnifiedVectorFormat rhs_child_data;
-	lhs_child.ToUnifiedFormat(lhs_list_size, lhs_child_data);
-	// rhs_child.ToUnifiedFormat(rhs_list_size, rhs_child_data);
+	UnifiedVectorFormat arrlist_data;
+	arrlist.ToUnifiedFormat(count, arrlist_data);
+	auto arrlist_entries = (list_entry_t *)arrlist_data.data;
+
+	auto arrlist_list_size = ListVector::GetListSize(arrlist);
+	auto &arrlist_child = ListVector::GetEntry(arrlist);
+	UnifiedVectorFormat arrlist_child_data;
+	arrlist_child.ToUnifiedFormat(arrlist_list_size, arrlist_child_data);
 
 	result.SetVectorType(VectorType::FLAT_VECTOR);
 	auto result_entries = FlatVector::GetData<list_entry_t>(result);
 	auto &result_validity = FlatVector::Validity(result);
 
 	// Create a reverse selection vector
-	SelectionVector rev1(lhs_list_size);
-	for (idx_t i = 0; i < lhs_list_size; i++) {
-		rev1.set_index(lhs_list_size - i - 1, i);
+	SelectionVector rev(arrlist_list_size);
+	for (idx_t i = 0; i < arrlist_list_size; i++) {
+		rev.set_index(arrlist_list_size - i - 1, i);
 	}
 
 	idx_t offset = 0;
 	for (idx_t i = 0; i < count; i++) {
-		auto lhs_list_index = lhs_data.sel->get_index(i);
+		auto arrlist_list_index = arrlist_data.sel->get_index(i);
 
-		// auto rhs_list_index = rhs_data.sel->get_index(i);
-		if (!lhs_data.validity.RowIsValid(lhs_list_index)
-		// && !rhs_data.validity.RowIsValid(rhs_list_index)
-		) {
+		if (!arrlist_data.validity.RowIsValid(arrlist_list_index)) {
 			result_validity.SetInvalid(i);
 			continue;
 		}
 		result_entries[i].offset = offset;
 		result_entries[i].length = 0;
-		if (lhs_data.validity.RowIsValid(lhs_list_index)) {
-			const auto &lhs_entry = lhs_entries[lhs_list_index];
-			result_entries[i].length += lhs_entry.length;
+		if (arrlist_data.validity.RowIsValid(arrlist_list_index)) {
+			const auto &arrlist_entry = arrlist_entries[arrlist_list_index];
+			result_entries[i].length += arrlist_entry.length;
 
 			// Create a reverse selection vector
-			// SelectionVector rev(lhs_entry.length);
-			// for (idx_t i = 0; i < lhs_entry.length; i++) {
-			// 	rev.set_index(lhs_entry.length - i - 1, i);
+			// SelectionVector rev(arrlist_entry.length);
+			// for (idx_t i = 0; i < arrlist_entry.length; i++) {
+			// 	rev.set_index(arrlist_entry.length - i - 1, i);
 			// }
-			ListVector::Append(result, lhs_child, rev1, lhs_entry.offset + lhs_entry.length,
-			                   lhs_entry.offset);
+			ListVector::Append(result, arrlist_child, rev, arrlist_entry.offset + arrlist_entry.length,
+			                   arrlist_entry.offset);
 		}
 
 
@@ -69,32 +67,40 @@ static void ListReverseFunction(DataChunk &args, ExpressionState &state, Vector 
 	}
 	D_ASSERT(ListVector::GetListSize(result) == offset);
 
-	if (lhs.GetVectorType() == VectorType::CONSTANT_VECTOR
-	//  && rhs.GetVectorType() == VectorType::CONSTANT_VECTOR
-	 ) {
+	if (arrlist.GetVectorType() == VectorType::CONSTANT_VECTOR) {
 		result.SetVectorType(VectorType::CONSTANT_VECTOR);
 	}
 }
-
 
 
 static unique_ptr<FunctionData> ListReverseBind(ClientContext &context, ScalarFunction &bound_function,
                                                vector<unique_ptr<Expression>> &arguments) {
 	D_ASSERT(bound_function.arguments.size() == 1);
 
-	auto &lhs = arguments[0]->return_type;
-	//auto &rhs = arguments[1]->return_type;
-	if (lhs.id() == LogicalTypeId::UNKNOWN 
-	// || rhs.id() == LogicalTypeId::UNKNOWN
-	) {
+
+
+		// case LogicalTypeId::SQLNULL:
+		// result.SetVectorType(VectorType::CONSTANT_VECTOR);
+		// ConstantVector::SetNull(result, true);
+		// break;
+
+
+
+	auto &arrlist = arguments[0]->return_type;
+	if (arrlist.id() == LogicalTypeId::UNKNOWN) {
 		throw ParameterNotResolvedException();
-	} else if (lhs.id() == LogicalTypeId::SQLNULL 
-	// || rhs.id() == LogicalTypeId::SQLNULL
-	) {
-		// we mimic postgres behaviour: list_id(NULL, my_list) = my_list
-		bound_function.arguments[0] = lhs;
-	} else {
-		D_ASSERT(lhs.id() == LogicalTypeId::LIST);
+	} 
+	else if (arrlist.id() == LogicalTypeId::SQLNULL) {
+		// arrlist = LogicalType::LIST(LogicalType::SQLNULL);
+		// throw ParameterNotResolvedException();
+
+		auto return_type = arrlist.id() == LogicalTypeId::SQLNULL ? arrlist : arrlist;
+		bound_function.arguments[0] = return_type;
+		//bound_function.arguments[1] = return_type;
+		bound_function.return_type = return_type;
+	} 
+	else {
+		D_ASSERT(arrlist.id() == LogicalTypeId::LIST);
 	
 		LogicalType child_type = LogicalType::SQLNULL;
 		for (const auto &argument : arguments) {
@@ -103,7 +109,6 @@ static unique_ptr<FunctionData> ListReverseBind(ClientContext &context, ScalarFu
 		auto list_type = LogicalType::LIST(child_type);
 
 		bound_function.arguments[0] = list_type;
-		// bound_function.arguments[1] = list_type;
 		bound_function.return_type = list_type;
 	}
 	return make_unique<VariableReturnBindData>(bound_function.return_type);
@@ -114,18 +119,15 @@ static unique_ptr<BaseStatistics> ListReverseStats(ClientContext &context, Funct
 	D_ASSERT(child_stats.size() == 2);
 
 	auto &left_stats = child_stats[0];
-	// auto &right_stats = child_stats[1];
 
 
 	auto stats = left_stats.ToUnique();
-	// stats->Merge(right_stats);
 
 	return stats;
 }
 
 ScalarFunction ListReverseFun::GetFunction() {
 	// the arguments and return types are actually set in the binder function
-	// this is where the arguments are defined
 	auto fun = ScalarFunction({LogicalType::LIST(LogicalType::ANY)},
 	                          LogicalType::LIST(LogicalType::ANY), ListReverseFunction, ListReverseBind, nullptr,
 	                          ListReverseStats);
